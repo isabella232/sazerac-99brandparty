b3dca431181ef8397f8b17a6bb561c9d3fe87977
Carsten Ziegeler <cziegeler@apache.org>
2023-09-15 16:28:24 +0200

    Ignore skip to content link

--- tools/importer/transformers/removeHeaderAndFooter.js
+++ tools/importer/transformers/removeHeaderAndFooter.js
@@ -1,4 +1,4 @@
 /* global WebImporter */
 export default function process(main) {
-  WebImporter.DOMUtils.remove(main, ['.elementor-location-header', '.elementor-location-footer', '.age_gate']);
+  WebImporter.DOMUtils.remove(main, ['.elementor-location-header', '.elementor-location-footer', '.age_gate', '.skip-link']);
 }
