7048bf7ae30405aad1b68e00adf52f5f859a25cf
Carsten Ziegeler <cziegele@adobe.com>
2023-09-29 16:49:39 +0200

    Styling of hard seltzer pairings (#60)
    * Styling of hard seltzer pairings
    
    * Use optimized images for background, make urls relative

--- blocks/liquer/liquer.js
+++ blocks/liquer/liquer.js
@@ -1,55 +1,17 @@
-import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
-import { fetchQueryIndex } from '../../scripts/scripts.js';
-
 export default async function decorate(block) {
-  const index = await fetchQueryIndex();
-  const list = block.querySelector('ul');
-  if (list) {
-    const nl = document.createElement('div');
-    nl.className = 'liquers';
-    let imageLeft = true;
-    list.querySelectorAll('li').forEach((li) => {
-      const item = document.createElement('div');
-      item.className = 'liquer-item';
-      item.classList.add(imageLeft ? 'left' : 'right');
-      nl.append(item);
-      const link = li.querySelector('a');
-      const path = new URL(link.href).pathname;
-      const recipe = index.data.find((r) => r.path === path);
-      if (recipe) {
-        const anchor = recipe.title.startsWith('99 ') ? recipe.title.substring(3) : null;
-        if (anchor) {
-          item.id = anchor.toLowerCase();
-        }
-        link.textContent = `See ${recipe.title} recipes`;
-        // create image
-        const img = createOptimizedPicture(recipe.image, true);
-        const imgDiv = document.createElement('div');
-        imgDiv.className = 'liquer-image';
-        imgDiv.append(img);
-        // create info div
-        const infoDiv = document.createElement('div');
-        infoDiv.className = 'liquer-info';
-        const title = document.createElement('h2');
-        title.textContent = recipe.title;
-        title.className = 'liquer-title';
-        infoDiv.append(title);
-        const desc = document.createElement('p');
-        desc.textContent = 'This is an AWESOME description';
-        desc.className = 'liquer-description';
-        infoDiv.append(desc);
-        infoDiv.append(link);
-        if (imageLeft) {
-          item.append(imgDiv);
-          item.append(infoDiv);
-        } else {
-          item.append(infoDiv);
-          item.append(imgDiv);
-        }
-      }
-      imageLeft = !imageLeft;
+  block.classList.add('liquers');
+  [...block.children].forEach((row, index) => {
+    row.className = 'liquer-item';
+    row.classList.add(index % 2 === 0 ? 'left' : 'right');
+    const link = row.querySelector('a');
+    link.classList.add('button');
+    const title = link.textContent;
+    const anchor = title.startsWith('99 ') ? title.substring(3) : null;
+    if (anchor) {
+      row.id = anchor.toLowerCase();
+    }
+    [...row.children].forEach((child, m) => {
+      child.classList.add(`liquer-part-${m}`);
     });
-    list.before(nl);
-    list.remove();
-  }
+  });
 }
