ba968c9630e38ffab91be6ced8cc11407d3300a5
Remo Liechti <141611642+RemoLiechti@users.noreply.github.com>
2023-10-18 10:55:10 +0200

    streamline pages that list products (#70)
    * add initial styling
    
    * linting
    
    * move new products to own theme and also slice out a table of products block
    
    * move all to new table of products block
    
    * rename block to clearly state where used
    
    * rename block to clearly state where used
    
    * styling

--- blocks/hard-seltzer/hard-seltzer.js
+++ blocks/hard-seltzer/hard-seltzer.js
@@ -1,7 +1,7 @@
 export default async function decorate(block) {
-  block.classList.add('liquers');
+  block.classList.add('hard-seltzers');
   [...block.children].forEach((row, index) => {
-    row.className = 'liquer-item';
+    row.className = 'hard-seltzer-item';
     row.classList.add(index % 2 === 0 ? 'left' : 'right');
     const link = row.querySelector('a');
     link.classList.add('button');
@@ -11,7 +11,7 @@ export default async function decorate(block) {
       row.id = anchor.toLowerCase();
     }
     [...row.children].forEach((child, m) => {
-      child.classList.add(`liquer-part-${m}`);
+      child.classList.add(`hard-seltzer-part-${m}`);
     });
   });
 }
