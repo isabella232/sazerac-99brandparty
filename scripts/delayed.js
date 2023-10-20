a231aab4d05fbe7b43960ab6dd82f58f1594a332
Karl Pauls <kpauls@adobe.com>
2023-10-20 11:51:45 +0200

    Add gtm and recapture (#79)

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,13 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './lib-franklin.js';
+import { loadScript, sampleRUM } from './lib-franklin.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+// GTM
+loadScript('/scripts/gtm-init.js', { defer: true });
+
+// reCapture
+loadScript('https://www.google.com/recaptcha/api.js?render=6LfrAQgeAAAAAP0zLUqJQydsBxkJp-XZK8KsKX_d&#038;ver=3.0', { defer: true });
